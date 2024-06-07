1c0f2607790b6fed79fd5c65b94eb7c229b3037a
Harsh <73820704+enthoharsh@users.noreply.github.com>
2024-06-07 10:23:16 +0530

    search

--- src/components/list-products/MenuItem.js
+++ src/components/list-products/MenuItem.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 const MenuItem = ({ name, price, description, customizable,onAddProduct,product }) => {
   return (
-    <div className="flex items-center justify-between py-4" style={{borderBottom:'1px dashed gray'}}>
-      <div>
-        <h3 className="text-sm font-semibold">{name}</h3>
-        <span className="text-sm font-semibold">₹{price}</span>
-        {description && <p className="text-sm text-gray-600">{description}</p>}
+    <div className="flex items-start justify-between py-4" style={{borderBottom:'1px dashed gray'}}>
+      <div className='flex flex-col items-start'>
+        <h3 className="text-sm font-semibold leading-8">{name}</h3>
+        <span className="text-sm font-semibold leading-8">₹{price}</span>
+        {description && <p className="text-sm text-gray-600 leading-8">{description}</p>}
       </div>
-      <div className="flex items-center">
-        <button onClick={()=>onAddProduct(product)} className="ml-4 px-4 py-2 bg-white shadow-md text-black font-light rounded-md active:bg-gray-200 transition-colors duration-200">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <img className='w-14 h-14 rounded' src={product.image_urls[0].url} alt='...' />
+        <button onClick={()=>onAddProduct(product)} className="px-4 py-2 bg-white shadow-md text-black font-light rounded-md active:bg-gray-200 transition-colors duration-200">
           ADD
         </button>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
                 <h4 className='mt-5 sticky'>What would you like to order?</h4>
                 <div className="grid grid-cols-2 gap-4 mt-5 overflow-auto h-[80vh]">
                     {ItemCategories.map((item) => (
-                        <Link to={`all-products?category=${item._id}`} key={item._id} className="relative">
+                        <Link to={`all-products?category=${item._id}&name=${item.name}`} key={item._id} className="relative">
                             <div className="relative h-52 overflow-hidden rounded-lg">
                                 <img
                                     src={item.image_url}
